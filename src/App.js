05bd80a91065581a7bcd54a7cce27f76862576c4
Muhammad Ismail <qureshiismail107@gmail.com>
2023-06-04 15:37:26 +0500

    my seconf commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './component/Home';
 import Navbar from './component/Navbar';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import Products from "./Products";
 
 export default function Home() {
   return (
     <div className="hero">
-      <div class="card text-bg-dark text-white border-0">
-        <img src="/assets/al-bg.png" class="card-img" alt="Background"  
-        height="550px"/>
-        <div class="card-img-overlay d-flex flex-column justify-content-center">
+      <div className="card text-bg-dark text-white border-0">
+        <img src="/assets/al-bg.png" className="card-img" alt="Background"  
+        height="300px"/>
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
             <div className="container">
-                <h5 class="card-title display-3 fw-bolder mb-0">Smoothy shirts</h5>
-                <p class="card-text lead fs-2">
+                <h5 className="card-title display-3 fw-bolder mb-0">Smoothy shirts</h5>
+                <p className="card-text lead fs-2">
                     Check out All Products
                 </p>
             </div>
         </div>
       </div>
+      <Products />
     </div>
   );
 }
